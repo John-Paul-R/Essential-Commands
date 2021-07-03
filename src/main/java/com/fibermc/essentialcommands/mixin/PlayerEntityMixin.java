e53cc888bc50ce3be55d5bb80ef9ca110a19ef0d
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-07-03 04:22:05 -0400

    Fix #21 via mixin magic. Mixin to getDisplayName on `ServerPlayerEntity` and not on `ClientPlayerEntity`. (Previously mixed in to PlayerEntityMixin, where the getDisplayName method is defined)

--- src/main/java/com/fibermc/essentialcommands/mixin/PlayerEntityMixin.java
+++ src/main/java/com/fibermc/essentialcommands/mixin/PlayerEntityMixin.java
@@ -17,43 +17,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Objects;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin implements PlayerEntityAccess {
-
-
-    @Unique
-    public PlayerData ecPlayerData;
+public class PlayerEntityMixin {
 
     @Inject(method = "getDisplayName", at = @At("RETURN"), cancellable = true)
     public void onGetDisplayName(CallbackInfoReturnable<Text> cir) {
-        try {
-            if (this.getEcPlayerData().getNickname() != null) {
 
-                cir.setReturnValue(new LiteralText("")
-                    .append(Config.NICKNAME_PREFIX)
-                    .append(this.getEcPlayerData().getNickname())
-                );
-                cir.cancel();
-            }
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
     }
 
-    @Override
-    public PlayerData getEcPlayerData() {
-        try {
-            return Objects.requireNonNull(ecPlayerData);
-        } catch (NullPointerException e) {
-            ServerPlayerEntity playerEntity = (ServerPlayerEntity)(Object)this;
-            EssentialCommands.LOGGER.warn(String.format("Player data did not exist for player with uuid '%s'. Creating it now.", playerEntity.getUuidAsString()));
-            PlayerData playerData = PlayerDataFactory.create(playerEntity);
-            setEcPlayerData(playerData);
-            return playerData;
-        }
-    }
-
-    @Override
-    public void setEcPlayerData(PlayerData playerData) {
-        ecPlayerData = playerData;
-    }
 }
