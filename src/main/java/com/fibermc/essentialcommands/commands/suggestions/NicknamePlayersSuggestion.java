2a9008dac85e7efe4f2ba2260b5e3183a5af9f8c
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-07-20 10:02:05 -0400

    Fix NPE bug in `/nickname reveal <nickname>`

--- src/main/java/com/fibermc/essentialcommands/commands/suggestions/NicknamePlayersSuggestion.java
+++ src/main/java/com/fibermc/essentialcommands/commands/suggestions/NicknamePlayersSuggestion.java
@@ -1,8 +1,10 @@
 package com.fibermc.essentialcommands.commands.suggestions;
 
+import com.fibermc.essentialcommands.PlayerData;
 import com.fibermc.essentialcommands.PlayerDataManager;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.MutableText;
 
 import java.util.Objects;
 
@@ -11,8 +13,9 @@ public class NicknamePlayersSuggestion {
     public static SuggestionProvider<ServerCommandSource> suggestedStrings() {
         return (context, builder) -> ListSuggestion.getSuggestionsBuilder(builder,
             PlayerDataManager.getInstance().getAllPlayerData().stream()
-                .map(playerData -> playerData.getNickname().getString())
+                .map(PlayerData::getNickname)
                 .filter(Objects::nonNull)
+                .map(MutableText::getString)
                 .sorted(String.CASE_INSENSITIVE_ORDER)
                 .toList()
         );
