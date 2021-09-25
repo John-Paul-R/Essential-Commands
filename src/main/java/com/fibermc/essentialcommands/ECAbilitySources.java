f98d4c4aab57b97420ebdf7eef6c4de9a8bff014
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-09-25 14:35:11 -0400

    IntelliJ Idea git client is real weird.

---
+++ src/main/java/com/fibermc/essentialcommands/ECAbilitySources.java
@@ -0,0 +1,9 @@
+package com.fibermc.essentialcommands;
+
+import io.github.ladysnake.pal.AbilitySource;
+import io.github.ladysnake.pal.Pal;
+import net.minecraft.util.Identifier;
+
+public class ECAbilitySources {
+    public static final AbilitySource FLY_COMMAND = Pal.getAbilitySource(EssentialCommands.MOD_ID, "ec-fly-command");
+}
