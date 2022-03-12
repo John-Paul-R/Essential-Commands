02b26e13d6a7f74c0bc3c4120386e80456f262cd
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-03-12 13:42:09 -0500

    Tabs to spaces.

--- src/main/java/com/fibermc/essentialcommands/EssentialCommands.java
+++ src/main/java/com/fibermc/essentialcommands/EssentialCommands.java
@@ -13,40 +13,40 @@ import org.apache.logging.log4j.Logger;
 import java.nio.file.Path;
 
 public final class EssentialCommands implements ModInitializer {
-	public static final ModMetadata MOD_METADATA = FabricLoader.getInstance().getModContainer("essential_commands").get().getMetadata();
-	public static final String MOD_ID = MOD_METADATA.getId();
-	public static final Logger LOGGER = LogManager.getLogger("EssentialCommands");
-	public static final EssentialCommandsConfig CONFIG = new EssentialCommandsConfig(
-		Path.of("./config/EssentialCommands.properties"),
-		"Essential Commands Config",
-		"https://github.com/John-Paul-R/Essential-Commands/wiki/Config-Documentation"
-	);
-	public static void log(Level level, String message) {
-		final String logPrefix = "[EssentialCommands]: ";
-		LOGGER.log(level, logPrefix.concat(message));
-	}
+    public static final ModMetadata MOD_METADATA = FabricLoader.getInstance().getModContainer("essential_commands").get().getMetadata();
+    public static final String MOD_ID = MOD_METADATA.getId();
+    public static final Logger LOGGER = LogManager.getLogger("EssentialCommands");
+    public static final EssentialCommandsConfig CONFIG = new EssentialCommandsConfig(
+        Path.of("./config/EssentialCommands.properties"),
+        "Essential Commands Config",
+        "https://github.com/John-Paul-R/Essential-Commands/wiki/Config-Documentation"
+    );
+    public static void log(Level level, String message) {
+        final String logPrefix = "[EssentialCommands]: ";
+        LOGGER.log(level, logPrefix.concat(message));
+    }
 
     @Override
-	public void onInitialize() {
-		log(Level.INFO, "Mod Load Initiated.");
+    public void onInitialize() {
+        log(Level.INFO, "Mod Load Initiated.");
 
-		CONFIG.loadOrCreateProperties();
+        CONFIG.loadOrCreateProperties();
 
-		ECPlaceholderRegistry.register();
+        ECPlaceholderRegistry.register();
 
-		ManagerLocator managers = ManagerLocator.getInstance();
-		managers.init();
-		ServerLifecycleEvents.SERVER_STARTING.register((server) -> {
-			managers.onServerStart(server);
-			ECPerms.init(); // ECPerms must start after WorldDataManager at present (for warps).
-		});
+        ManagerLocator managers = ManagerLocator.getInstance();
+        managers.init();
+        ServerLifecycleEvents.SERVER_STARTING.register((server) -> {
+            managers.onServerStart(server);
+            ECPerms.init(); // ECPerms must start after WorldDataManager at present (for warps).
+        });
 
-		CommandRegistrationCallback.EVENT.register(EssentialCommandRegistry::register);
+        CommandRegistrationCallback.EVENT.register(EssentialCommandRegistry::register);
 
-		if (CONFIG.CHECK_FOR_UPDATES.getValue()) {
-			Updater.checkForUpdates();
-		}
+        if (CONFIG.CHECK_FOR_UPDATES.getValue()) {
+            Updater.checkForUpdates();
+        }
 
-		log(Level.INFO, "Mod Load Complete.");
-	}
+        log(Level.INFO, "Mod Load Complete.");
+    }
 }
