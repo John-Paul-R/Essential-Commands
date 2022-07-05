16c975d1e6afd271d7a50a085fe21ffac08f48a6
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-07-04 22:56:03 -0400

    Persist RTP cooldown to player data file. (Resolves #106)

--- src/main/java/com/fibermc/essentialcommands/PlayerData.java
+++ src/main/java/com/fibermc/essentialcommands/PlayerData.java
@@ -184,6 +184,10 @@ public class PlayerData extends PersistentState {
             }
         }
 
+        if (dataTag.contains("rtpNextUsableTime")) {
+            this.rtpNextUsableTime = dataTag.getInt("rtpNextUsableTime");
+        }
+
         if (this.player != null) {
             updatePlayer(this.player);
         }
@@ -199,6 +203,8 @@ public class PlayerData extends PersistentState {
         tag.put("homes", homesNbt);
 
         tag.putString("nickname", Text.Serializer.toJson(nickname));
+
+        tag.putInt("rtpNextUsableTime", rtpNextUsableTime);
 
         return tag;
     }
