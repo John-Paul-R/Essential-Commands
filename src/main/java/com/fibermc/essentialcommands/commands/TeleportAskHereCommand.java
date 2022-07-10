7da0665d39a684a955ddb768e1eb7a5f383d0b4f
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-07-09 22:27:33 -0400

    Add existingTeleportRequest check for `/tpahere` (Resolves #111)

--- src/main/java/com/fibermc/essentialcommands/commands/TeleportAskHereCommand.java
+++ src/main/java/com/fibermc/essentialcommands/commands/TeleportAskHereCommand.java
@@ -1,6 +1,7 @@
 package com.fibermc.essentialcommands.commands;
 
 import com.fibermc.essentialcommands.*;
+import com.fibermc.essentialcommands.access.ServerPlayerEntityAccess;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
@@ -22,6 +23,21 @@ public class TeleportAskHereCommand implements Command<ServerCommandSource> {
         TeleportRequestManager tpMgr = ManagerLocator.getInstance().getTpManager();
         ServerPlayerEntity senderPlayer = context.getSource().getPlayer();
         ServerPlayerEntity targetPlayer = EntityArgumentType.getPlayer(context, "target_player");
+
+        // Don't allow spamming same target.
+        {
+            var existingTeleportRequest = ((ServerPlayerEntityAccess) senderPlayer)
+                .getEcPlayerData()
+                .getSentTeleportRequest();
+            if (existingTeleportRequest != null && existingTeleportRequest.getTargetPlayer().equals(targetPlayer)) {
+                senderPlayer.sendMessage(
+                    ECText.getInstance().getText(
+                        "cmd.tpask.error.exists",
+                        existingTeleportRequest.getTargetPlayer().getDisplayName())
+                    , MessageType.SYSTEM);
+                return 0;
+            }
+        }
 
         //inform target player of tp request via chat
         targetPlayer.sendMessage(TextUtil.concat(
