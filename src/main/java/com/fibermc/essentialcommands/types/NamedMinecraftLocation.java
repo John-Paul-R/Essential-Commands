bac458edfaa90df6c249756c6527b3395576c168
v2ray <60914079+LagPixelLOL@users.noreply.github.com>
2022-12-08 21:32:37 +0800

    Update NamedMinecraftLocation.java

--- src/main/java/com/fibermc/essentialcommands/types/NamedMinecraftLocation.java
+++ src/main/java/com/fibermc/essentialcommands/types/NamedMinecraftLocation.java
@@ -5,7 +5,7 @@ import com.fibermc.essentialcommands.text.TextFormatType;
 
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
-import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.World;
 
 import dev.jpcode.eccore.util.TextUtil;
