874b4be01279ec372dde18b788f01df07fb27110
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-03-18 12:13:25 -0400

    don't use Text for the RTP LOGGER message

--- src/main/java/com/fibermc/essentialcommands/commands/RandomTeleportCommand.java
+++ src/main/java/com/fibermc/essentialcommands/commands/RandomTeleportCommand.java
@@ -89,7 +89,7 @@ public class RandomTeleportCommand implements Command<ServerCommandSource> {
                         EssentialCommands.LOGGER.info(
                             String.format(
                                 "Total RTP Time: %s",
-                                Text.literal(String.valueOf(totalTime))
+                                totalTime
                             ));
                     }
                 } catch (CommandSyntaxException e) {
