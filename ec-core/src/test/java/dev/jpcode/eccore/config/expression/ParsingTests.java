63e31c694eda3bffafb335fe9b0c1c71947d44fa
John Paul <jp@jpcode.dev>
2023-04-02 19:51:33 -0400

    Add config `Expression`s (#199)
    * Add snippet from Apache CollectionUtils
    
    * Expression parsing: initial commit
    
    * Don't recursively create new readers. Private method that accepts reader instead.
    
    * fixup access modifiers, minor readability
    
    * convert the Actions into methods on `ParsingContext`
    
    * match LogicalOperator w/ switch instead of if
    
    * Remove ExpressionOperand, replace with base Expression. Rename PatternMatchingExpression to BinaryExpression.
    
    * Fix parsing bugs. Add basic expression evaluator (Expression.matches(), Collection evaluation context)

---
+++ ec-core/src/test/java/dev/jpcode/eccore/config/expression/ParsingTests.java
@@ -0,0 +1,52 @@
+package dev.jpcode.eccore.config.expression;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.gradle.internal.impldep.org.junit.Assert.assertEquals;
+
+@DisplayName("ParsingTests")
+public class ParsingTests {
+
+    @Test
+    @DisplayName("PatternMatchingExpressionReader parse - base")
+    void PatternMatchingExpressionReader_parse_parsesCorrectly() {
+        String input = "1 OR 2 AND (3 OR 4)";
+
+        var retVal = PatternMatchingExpressionReader.parse(input, Integer::parseInt);
+
+        var outStr = retVal.serialize();
+        assertEquals("(1 OR (2 AND (3 OR 4)))", outStr);
+    }
+
+    private enum PlayerProperties
+    {
+        IsFast,
+        CanHighJump,
+        IsAncient,
+        CanFly,
+        IsInvisible,
+        IsLarge,
+    }
+
+    @Test
+    void PatternMatchingExpressionReader_matches_matchesCorrectly() {
+        String isLegendaryExpression = "CanFly AND IsInvisible OR IsLarge AND IsAncient";
+
+        var expression = PatternMatchingExpressionReader.parse(isLegendaryExpression, PlayerProperties::valueOf);
+
+        var playerPropertiesContext = CollectionExpressionEvaluationContext.from(
+            PlayerProperties.IsAncient,
+            PlayerProperties.IsLarge
+        );
+
+        var outStr = expression.serialize();
+        assertEquals("((CanFly AND IsInvisible) OR (IsLarge AND IsAncient))", outStr);
+
+        var playerIsLegandary = expression.matches(playerPropertiesContext);
+
+        assertEquals(true, playerIsLegandary);
+
+    }
+
+}
