a4901f6757a4379672f357219bb2aea3e19c4e59
Disy <72013933+disymayufei@users.noreply.github.com>
2023-07-02 00:05:55 +0800

    feat: Spectator cannot back to the previous location (#221)
    Co-authored-by: John Paul <jp@jpcode.dev>

--- src/main/java/com/fibermc/essentialcommands/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/fibermc/essentialcommands/mixin/ServerPlayerEntityMixin.java
@@ -36,6 +36,9 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntityMixin implemen
     @Shadow
     public abstract ServerWorld getWorld();
 
+    @Shadow
+    public abstract boolean isSpectator();
+
     @Unique
     public QueuedTeleport ecQueuedTeleport;
 
@@ -165,7 +168,9 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntityMixin implemen
     // Teleport hook (for /back)
     @Inject(method = "teleport(Lnet/minecraft/server/world/ServerWorld;DDDFF)V", at = @At("HEAD"))
     public void onTeleport(ServerWorld targetWorld, double x, double y, double z, float yaw, float pitch, CallbackInfo ci) {
-        this.ec$getPlayerData().setPreviousLocation(new MinecraftLocation((ServerPlayerEntity) (Object) this));
+        if (!isSpectator()) {
+            this.ec$getPlayerData().setPreviousLocation(new MinecraftLocation((ServerPlayerEntity) (Object) this));
+        }
     }
 
     @Inject(method = "enterCombat", at = @At("RETURN"))
